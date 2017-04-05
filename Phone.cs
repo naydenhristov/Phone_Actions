420e1214e8daf2dd9fc65d5e2f568fa55dee8f03
Nayden Hristov <n_hristov@mail.bg>
2017-04-05 19:34:14 +0300

    Phone Actions - Practicing Strings Arrays and Methods

---
+++ Phone.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Linq;
+
+namespace Task2_4_Phone
+{
+    class Phone
+    {
+        public static void Main()
+        {
+            string[] phoneNumbers = Console.ReadLine()
+                .Split(' ')
+                .ToArray();
+
+            string[] names = Console.ReadLine()
+                .Split(' ')
+                .ToArray();
+
+            string[] command = Console.ReadLine()
+                .Split(' ')
+                .ToArray();
+
+            string action = string.Empty;
+
+            while (command[0] != "done")
+            {
+                switch (command[0])
+                {
+                    case "call": 
+                        action = Call(command[1], names, phoneNumbers);
+                        Console.WriteLine(action);
+                        break;
+                    case "message" :
+                        action = Message(command[1], names, phoneNumbers);
+                        Console.WriteLine(action);
+                        break;
+                    default:
+                        break;
+                }
+
+                command = Console.ReadLine()
+                .Split(' ')
+                .ToArray();
+            }
+        }
+
+        private static string Call(string nameOrPhoneNumber, string[] name, string[] phoneNumber)
+        {
+            string callResult = string.Empty;
+            string oppositeElement = string.Empty;
+            int sumOfDigits = 0;
+
+            for (int i = 0; i < name.Length; i++)
+            {
+                if (nameOrPhoneNumber == name[i])
+                {
+                    oppositeElement = phoneNumber[i];
+                    sumOfDigits = SumOfDigits(phoneNumber[i]);
+                }
+                else if (nameOrPhoneNumber == phoneNumber[i])
+                {
+                    oppositeElement = name[i];
+                    sumOfDigits = SumOfDigits(phoneNumber[i]);
+                }
+            }
+
+            int minutes = sumOfDigits / 60;
+            int seconds = sumOfDigits % 60;
+
+            if (sumOfDigits % 2 == 0)
+            {
+                callResult = "calling " + oppositeElement + "..." + Environment.NewLine + 
+                    "call ended. duration: " + minutes.ToString("D2") + ":" + seconds.ToString("D2");
+            }
+            else
+            {
+                callResult = "calling " + oppositeElement + "..." + Environment.NewLine + "no answer";
+            }
+
+            return callResult;
+        }
+
+        private static string Message(string nameOrPhoneNumber, string[] name, string[] phoneNumber)
+        {
+            string messageResult = string.Empty;
+            string oppositeElement = string.Empty;
+            int differenceOfDigits = 0;
+            
+            for (int i = 0; i < name.Length; i++)
+            {
+                if (nameOrPhoneNumber == name[i])
+                {
+                    oppositeElement = phoneNumber[i];
+                    differenceOfDigits = DifferenceOfDigits(phoneNumber[i]);
+                }
+                else if (nameOrPhoneNumber == phoneNumber[i])
+                {
+                    oppositeElement = name[i];
+                    differenceOfDigits = DifferenceOfDigits(phoneNumber[i]);
+                }
+            }
+
+            if (differenceOfDigits % 2 == 0)
+            {
+                messageResult = "sending sms to " + oppositeElement + "..." + Environment.NewLine + "meet me there";
+            }
+            else
+            {
+                messageResult = "sending sms to " + oppositeElement + "..." + Environment.NewLine + "busy";
+            }
+
+            return messageResult;
+        }
+
+        private static int DifferenceOfDigits(string p)
+        {
+            int digit = 0;
+            int difference = 0;
+
+            foreach (char symbol in p)
+            {
+                digit = symbol - '0';
+                if (digit >= 0 && digit <= 9)
+                {
+                    difference -= digit;
+                }
+            }
+
+            return Math.Abs(difference);
+        }
+
+        private static int SumOfDigits(string p)
+        {
+            int digit = 0;
+            int sum = 0;
+
+            foreach (char symbol in p)
+            {
+                digit = symbol - '0';
+                if (digit >= 0 && digit <= 9)
+                {
+                    sum += digit;
+                }
+            }
+
+            return sum;
+        }
+       
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,74 @@
 # Phone_Actions
 Practice Strings Arrays and Methods 
+
+Description:
+You will be given an array of phone numbers (strings) and an array of names (strings). After which, you will be given strings on new lines, until you receive the command “done”. 
+
+A number corresponds to a name when it’s located on the same position as its corresponding name in both arrays.
+
+Implement a phone with this functionality:
+- call {number/name} -> print “calling {name/number}...”
+- If the sum of the digits (ignore other characters) of the number is odd, print “no answer”.
+- If the sum of the digits (ignore other characters) of the number is even, print “call ended. duration: {duration}”. The duration is calculated from the sum of the digits in the format “mm:ss”
+
+- message {number/name} -> print “sending sms to {name/number}...”
+- if the difference of the digits (ignore other characters) of the number is odd, print “busy”
+- if the difference of the digits (ignore other characters) of the number is even, print “meet me there”
+
+Constraints:
+- The numbers will contain the digits 0-9 and might contain the plus sign "+". They will always be valid and in this format.
+- The numbers and names will be unique – there will be no cases of repeated numbers or repeated names.
+- The names and numbers will always be separated by exactly one whitespace.
+
+Examples:
+
+Input:
+(602)207-2544 +918671613200 1-661-335-8202
+John Prakash Felix
+call Prakash
+call John
+call Felix
+done
+
+
+Output:
+calling +918671613200
+call ended. duration: 00:44
+calling (602)207-2544
+call ended. duration: 00:32
+calling 1-661-335-8202
+no answer
+---------------------------------
+
+Input:
+0882983983 0822223321 0898128398 0878223211
+Vasil Hristo Georgi Luben
+message Vasil
+message 0878223211
+message Hristo
+done
+
+Output:
+sending sms to 0882983983...
+meet me there
+sending sms to Luben
+meet me there
+sending sms to 0822223321...
+busy
+---------------------------------
+
+Input:
+0888888888 0888123456 +359886001122
+Nikolai Ivan Maria
+call 0888888888
+call Ivan
+message Maria
+done
+
+Output:
+calling Nikolai...
+call ended. duration: 01:12
+calling 0888123456...
+no answer
+sending sms to +359886001122...
+busy
